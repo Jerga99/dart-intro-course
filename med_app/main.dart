a7741ae00d9b1ab4bfbbf3bf792070213de29048
Filip Jerga <jerga99@gmail.com>
2019-05-17 19:48:56 +0200

    26 List Functions

--- med_app/main.dart
+++ med_app/main.dart
@@ -6,18 +6,31 @@ import './models/person.dart';
 void main() {
   List<String> person1Allergies = ['peanuts', 'wheat', 'apples'];
   List<String> person2Allergies = ['dust', 'oranges', 'cats'];
-  Person person1 = Person('Filip Jerga', 28, 187.5, true, person1Allergies);
-  Person person2
-    = Person.named(name: 'John Smith',
-                   age: 35,
-                   height: 177.8,
-                   employeeStatus:  false,
-                   allergies: person2Allergies);
 
-  person1.displayUserInfo();
-  person2.displayUserInfo();
+  print(person1Allergies[0]);
+  print(person1Allergies.elementAt(0));
 
-  print(person1.getAgeInDays());
+  person1Allergies.add('dog');
+  person1Allergies.addAll(['ducks', 'mouse']);
+  person1Allergies.remove('peanuts');
+
+  var allergyIndex = person1Allergies.indexOf('wheat');
+  person1Allergies.removeAt(allergyIndex);
+
+  print(person1Allergies);
+
+  // Person person1 = Person('Filip Jerga', 28, 187.5, true, person1Allergies);
+  // Person person2
+  //   = Person.named(name: 'John Smith',
+  //                  age: 35,
+  //                  height: 177.8,
+  //                  employeeStatus:  false,
+  //                  allergies: person2Allergies);
+
+  // person1.displayUserInfo();
+  // person2.displayUserInfo();
+
+  // print(person1.getAgeInDays());
 }
 
 
